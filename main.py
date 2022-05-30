a617842f923488c350f80a8189943e9a6561cc88
Abhinav KM <abhinav.m@zerone-consulting.com>
2022-05-30 12:28:41 +0530

    Data files for more subjects

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cmath import exp
 from numpy import kaiser
 import streamlit as st
 import pandas as pd
@@ -15,19 +16,6 @@ df = pd.read_csv("./resources/csv/sanitized.csv")
 
 jsonPath = "./resources/findings/"
 
-# save last state
-# def loadSaveState(_data=None, _mode="r+"):
-#     with open(path.join(jsonPath, "restore.json"), _mode) as state:
-#         if not _data:
-#             last_choice = load(state)
-#             print("You stoppd at:", last_choice)
-#         else:
-#             dump(_data, state, indent=4)
-#             last_choice = _data
-#         return last_choice
-
-
-# last_state = loadSaveState()
 
 # list of subjects
 subjects = df.Subject.unique()
@@ -40,11 +28,6 @@ excluded_subjects = []
 st.sidebar.header("Filter")
 subject_choice = st.sidebar.selectbox("Subject:", subjects)
 
-# last_state["subject"] = subject_choice
-# loadSaveState(_data=last_state, _mode="w")
-
-# if choice := last_state["subject"]:
-#     subject_choice = choice
 
 subject = df["Subject"] = subject_choice
 
@@ -55,19 +38,12 @@ file_choice = st.sidebar.selectbox("Data:", files)
 
 
 def viewFile(_path, _file):
-    # if _file != "full-data.json":
-    #     last_state["file"] = _file
-    #     loadSaveState(_data=last_state, _mode="w")
 
     with open(path.join(_path, _file)) as fp:
         return fp.read()
 
 
-# if choice := last_state["file"]:
-#     if choice != "full-data.json":
-#         file_choice = choice
-
-st.sidebar.json(viewFile(path.join(jsonPath, "json"), file_choice))
+st.sidebar.json(viewFile(path.join(jsonPath, "json"), file_choice), expanded=False)
 
 file_name = st.sidebar.text_input(label="New file", key="file_name")
 
@@ -76,9 +52,11 @@ compile_file = st.sidebar.button(label="Compile")
 
 
 if save_file:
-    with st.spinner("Creating file.."):
-        sleep(1)
-    createJSON(fileName=file_name)
+    if file_name in subjects:
+        with st.spinner("Creating file.."):
+            sleep(1)
+            createJSON(fileName=file_name)
+    st.sidebar.error("Filename must be a subject")
 
 if compile_file:
     with st.spinner("Writing to file.."):
@@ -90,5 +68,5 @@ final_df = df[["Subject", "Journal Abstract Text"]]
 
 st.dataframe(final_df, 500, 500)
 
-with st.expander("Full data"):
-    st.json(viewFile("./resources/findings/", "full-data.json"))
+with st.expander(f"Compiled data ({len(files)})"):
+    st.json(viewFile("./resources/findings/", "full-data.json"), expanded=False)
